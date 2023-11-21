d0deeda83ba7dcd73a1aa51c8a366f3e72cf2e22
Arthur Beck <averse.abfun@gmail.com>
2023-11-21 04:19:54 +0000

    Pending changes exported from your codespace

--- constants.py
+++ constants.py
@@ -1,22 +1,50 @@
-from enum import Enum, auto
+"""the constants"""
 
-class Room(Enum):
-    EMPTY = auto()
-    START = auto()
-    
-    CORRIDOR22 = auto()
-    CORRIDOR32 = auto()
-    CORRIDOR42 = auto()
-    CORRIDOR23 = auto()
-    CORRIDOR43 = auto()
-    CORRIDOR24 = auto()
-    CORRIDOR44 = auto()
-    CORRIDOR25 = auto()
-    CORRIDOR45 = auto()
-    
-    LUNCHROOM13 = auto()
-    LAB54 = auto()
-    ENGINES14 = auto()
-    ENGINES54 = auto()
-    PLANT26 = auto()
-    WATERGEN46 = auto()
+EMPTY = (-1,-1)
+START = (-2,-2)
+
+CORRIDOR22 = (2,2)
+CORRIDOR32 = (3,2)
+CORRIDOR42 = (4,2)
+CORRIDOR23 = (2,3)
+CORRIDOR43 = (4,3)
+CORRIDOR24 = (2,4)
+CORRIDOR44 = (4,4)
+CORRIDOR25 = (2,5)
+CORRIDOR45 = (4,5)
+
+LUNCHROOM13 = (1,3)
+LAB54 = (5,4)
+ENGINES14 = (1,4)
+ENGINES54 = (5,4)
+PLANT26 = (2,6)
+WATERGEN46 = (4,6)
+BEDROOM = (3,1)
+
+MAIN_HTML = \
+"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>Space Snack</title>
+<style>
+* {
+    font-family: Roboto;
+    background-color: black;
+    color: white;
+}
+.page {
+    position: absolute;
+    top: -500em;
+    transition: top 1s;
+}
+.page:target {
+    top: 0em;
+}
+</style>
+</head>
+<body>
+[[CODE]]
+</body>
+</html>
+"""
--- generate.py
+++ generate.py
@@ -1,18 +1,35 @@
+"""the actual generation code"""
+
 import csv
 import constants
 
-inventory = 0b0
+def parseItems(data):
+    """parse the data in EVENTS and EXITS"""
+    if not data:
+        return {}
+    data = data.split(",")
+    ret = {}
+    for item in data:
+        item = item.split(":")
+        ret[item[0].lower()] = item[1]
+    return ret
+
+inventory = 0
 START = (3,1)
 (x,y) = START
-room = constants.Room.START
+room = constants.START
 map = csv.reader(open("map.csv","r", newline=''))
-tmpmap = []
+tmpmap = {}
 for i, row in enumerate(map):
     if i==0: continue
-    tmpmap.append(row[1:])
+    tmpmap[row[0]] = {"description":row[1],"events":parseItems(row[2]),"exits":parseItems(row[3])}
 map = tmpmap
 del tmpmap
-print(map)
+
+page = constants.MAIN_HTML
+eventsActive = []
+def HAVE_GOT_ALL(data):
+    return data
 
 def setInventory(value):
     global inventory
@@ -25,5 +42,19 @@ def removeInventory(value):
         inventory = inventory - value
 
 def generatePageId():
-    global inventory, room
+    global inventory
     return f"{inventory}{x}{y}"
+
+def generatePage(room, changes=lambda code : code):
+    global page
+    code = "<div class=\"page\">"
+    code += room["description"]
+    code = changes(code)
+    for key in room["events"].keys():
+        if key in eventsActive:
+            code = globals()[room["events"][key]](code)
+    code += "</div>\n[[CODE]]"
+    page = page.replace("[[CODE]]", code)
+    return page
+
+print(generatePage(map["START"]))
